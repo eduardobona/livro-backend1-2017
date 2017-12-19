092e75969a702c5574310748b6dfce9b48f52cbf
Eduardo Bona <eduardobona@gmail.com>
2017-12-19 18:05:26 -0200

    criando array de informações para include

---
+++ dados/empresa_departamento.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+  0 => array(
+    'empresa' => 'GB EAD',
+    'departamentos' => array(
+      'autor' => 'autor@gb.com.br',
+      'aulas' => 'aulas@gb.com.br',
+      'comercial' => 'comercial@gb.com.br',    
+    )
+  ),
+  1 => array(
+    'empresa' => 'GB Palestras',
+    'departamentos' => array(
+      'comercial' => 'comercial@gb.com.br',
+      'eventos' => 'eventos@gb.com.br'
+    )
+  )
+);
+?>
