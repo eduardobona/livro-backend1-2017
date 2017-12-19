c2ff1a8a0221ec49d983e9bb58fcd35b66c4ea3c
Eduardo Bona <eduardobona@gmail.com>
2017-12-19 17:47:08 -0200

    criando arquivo index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Grupo Bona - Currículo Online</title>
+    <link href="css/estilo.css" type="text/css" />
+  </head>
+  <body>
+    <div class="central">           
+      <div class="cabecalho">
+        <img src="img/logo.png" />
+      </div>            
+      <div class="principal">
+        <div class="lista-empresas">
+          <h3>Escolha para qual empresa do grupo deseja enviar seu currículo:</h3>
+          <ul>
+            <li><a href="curriculo.php">GB EAD</a></li>
+            <li><a href="curriculo.php">GB Palestras</a></li>
+          </ul>
+        </div>
+      </div><!-- fim principal -->           
+    </div><!-- fim central -->
+  </body>
+</html>
