4a219114d03710c648a6a0ac20791591971c6cf0
Eduardo Bona <eduardobona@gmail.com>
2017-12-19 17:47:52 -0200

    separando índex em cabeçalho

---
+++ layout/cabeçalho.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Grupo Bona - Currículo Online</title>
+    <link href="css/estilo.css" type="text/css" />
+  </head>
+  <body>
+    <div class="central">           
+      <div class="cabecalho">
+        <img src="img/logo.png" />
+      </div>            
+      <div class="principal">
