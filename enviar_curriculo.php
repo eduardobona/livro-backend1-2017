6ba74e71861875791f95e936885dc1118a0ee331
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2017-12-20 09:14:09 -0200

    adicionando funcao para mover arquivos no script de enviar curriculo

--- enviar_curriculo.php
+++ enviar_curriculo.php
@@ -1,4 +1,5 @@
 <?php
+  // recebendo dados
   $dados = $_POST;
   include 'funcoes/validadores.php';
   if (validar_curriculo($dados) == false) {
@@ -6,8 +7,17 @@
 $dados['empresa']);
     exit();
   }
+
+  // recebendo dados em variáveis
   $nome = $dados['nome'];
   $email = $dados['email'];
   $cod_departamento = $dados['departamento'];
   $cod_empresa = $dados['empresa'];
+
+  // realizando o upload
+  include 'funcoes/arquivos.php';
+
+  $foto = mover_arquivo($arquivos['foto'], 'foto');
+  $curriculo = mover_arquivo($arquivos['curriculo'], 'curriculo');
+
 ?>
