5afa5af0f6b1f2f34ebb3bd314a4d161fbc9918b
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2017-12-20 14:54:12 -0200

    implementando regras de negócios em script enviar_currilo.php

--- enviar_curriculo.php
+++ enviar_curriculo.php
@@ -1,10 +1,12 @@
 <?php
-  // recebendo dados
+
+  // recebendo e validando dados
   $dados = $_POST;
   include 'funcoes/validadores.php';
-  if (validar_curriculo($dados) == false) {
-    header('location: curriculo.php?erro=erro&codigo=' .
-$dados['empresa']);
+  $validacao = validar_curriculo($dados);
+  if ($validacao !== true) {
+    header('location: curriculo.php?erro='.
+      $validacao.'&codigo=' . $dados['empresa']);
     exit();
   }
 
@@ -14,10 +16,43 @@ $dados['empresa']);
   $cod_departamento = $dados['departamento'];
   $cod_empresa = $dados['empresa'];
 
+  // validando arquivos
+  $arquivos = $_FILES;
+  $validacao = validar_curriculo_arquivos($arquivos);
+  if ($validacao !== true) {
+    header('location: curriculo.php?erro='.
+      $validacao.'&codigo=' . $dados['empresa']);
+    exit();
+  }
+
   // realizando o upload
   include 'funcoes/arquivos.php';
 
   $foto = mover_arquivo($arquivos['foto'], 'foto');
+  if ($foto === false) {
+    $mensagem_erro = "Erro ao enviar foto";
+    header('location: curriculo.php?erro='.
+           $mensagem_erro . '&codigo=' . $dados['empresa']);
+    exit();
+  }
+
   $curriculo = mover_arquivo($arquivos['curriculo'], 'curriculo');
+  if ($curriculo === false) {
+    $mensagem_erro = "Erro ao enviar currículo";
+    header('location: curriculo.php?erro='.
+           $mensagem_erro . '&codigo=' . $dados['empresa']);
+    exit();
+  }
+
+  // enviando email e concluindo cadastro
+  include 'funcoes/emails.php';
+  agradecer_contato($nome, $email);
+  enviar_curriculo_email(
+    array('nome' => $nome, 'email' => $email),
+    array('foto' => $foto, 'curriculo' => $curriculo)
+  );
+
+  header('location: curriculo_cadastrado.php');
+  exit();
 
 ?>
