fa8e34f04de42700be8628e1ff3675f3ad8fdc5a
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2017-12-20 09:04:39 -0200

    alterando indices de 0 e 1 para 1 e 2 evitando assim conversão para 0 no caso da não passagem do parâmetro

--- dados/empresa_departamento.php
+++ dados/empresa_departamento.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-  0 => array(
+  1 => array(
     'empresa' => 'GB EAD',
     'departamentos' => array(
       'autor' => 'autor@gb.com.br',
@@ -8,7 +8,7 @@ return array(
       'comercial' => 'comercial@gb.com.br',    
     )
   ),
-  1 => array(
+  2 => array(
     'empresa' => 'GB Palestras',
     'departamentos' => array(
       'comercial' => 'comercial@gb.com.br',
