6b8beac52c049e0d5ae43669a5e913e05c61bc49
Eduardo Bona <eduardobona@gmail.com>
2017-12-20 08:55:45 -0200

    criando script para enviar currículo

---
+++ enviar_curriculo.php
@@ -0,0 +1,13 @@
+<?php
+  $dados = $_POST;
+  include 'funcoes/validadores.php';
+  if (validar_curriculo($dados) == false) {
+    header('location: curriculo.php?codigo=' .
+$dados['empresa']);
+    exit();
+  }
+  $nome = $dados['nome'];
+  $email = $dados['email'];
+  $cod_departamento = $dados['departamento'];
+  $cod_empresa = $dados['empresa'];
+?>
