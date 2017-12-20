e99177de0451c62481acc60beff1f105c0b98b9d
Eduardo Bona <eduardobona@gmail.com>
2017-12-20 08:29:43 -0200

    criando arquivo para envio de currículo

---
+++ currículo.php
@@ -0,0 +1,36 @@
+<?php
+  include 'layout/cabecalho.php';
+  $empresa = $_GET['codigo'];
+?>
+        <div class="formulario-curriculo">
+          <h3>Envie seu currículo com foto:</h3>
+          <form action="enviar_curriculo.php" method="post">
+            <label>Nome:</label>
+            <input type="text" name="nome" />
+            <label>Email:</label>
+            <input type="email" name="email" />
+            <label>Departamento:</label>
+            <select name="departamento">
+<?php
+  $empresa_departamento = include 'dados/empresa_departamento.php';
+  $departamentos = $empresa_departamento[$empresa]['departamentos'];
+  foreach ($departamentos as $indice => $departamento) {
+?>
+
+              <option value="<?php echo $indice?>"><?php echo ucfirst($indice) ?></option>
+
+<?php
+  }
+?>
+            </select>
+            <label>Foto:</label>
+            <input type="file" name="foto" />
+            <label>Curriculo:</label>
+            <input type="file" name="curriculo" />
+            <input type="hidden" name="empresa" value="<?php echo $empresa?>" />        
+            <input type="submit" value="Enviar" />
+          </form>
+        </div>
+<?php
+  include 'layout/rodape.php';
+?>
