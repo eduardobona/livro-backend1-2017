1f9b286d4a369d247991b07a61f2dfc29f8914f0
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2017-12-20 13:54:40 -0200

    criando pagina para finalizar cadastro

---
+++ curriculo_cadastrado.php
@@ -0,0 +1,10 @@
+<?php
+  include 'layout/cabecalho.php';
+?>
+
+<h3>Obrigado pelo seu cadastro.</h3>
+<h3>Esperamos em breve poder entrar em contato.</h3>
+
+<?php
+  include 'layout/rodape.php';
+?>
