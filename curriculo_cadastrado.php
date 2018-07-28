dc915257d12fda0b288addd312c5157aa8cb114d
Elton Mel <eltondemel2010@hotmail.com>
2018-07-28 20:14:27 -0300

    Incluido link para retornar a página inicial

--- curriculo_cadastrado.php
+++ curriculo_cadastrado.php
@@ -4,6 +4,7 @@
 
 <h3>Obrigado pelo seu cadastro.</h3>
 <h3>Esperamos em breve poder entrar em contato.</h3>
+<a href="index.php" class="link">Página inicial</a>
 
 <?php
   include 'layout/rodape.php';
