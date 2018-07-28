e1bf54567dd2bc4a5e3e97cd60a0c072a2c0c877
Elton Mel <eltondemel2010@hotmail.com>
2018-07-28 20:32:28 -0300

    Alterada tag IMG pois faltava o atribuito ALT

--- layout/cabecalho.php
+++ layout/cabecalho.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charset="UTF-8" />
     <title>Grupo Bona - Currículo Online</title>
     <link rel="stylesheet" href="css/estilo.css" type="text/css" />
@@ -8,6 +9,6 @@
   <body>
     <div class="central">           
       <div class="cabecalho">
-        <img src="img/logo.png" />
+          <img src="img/logo.png" alt="Logo" />
       </div>            
       <div class="principal">
